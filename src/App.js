53b0599dc1cb513293aecc22ac6768059eda4ccc
Nat Dean-Lewis <Nat.Dean-Lewis@softwire.com>
2022-05-13 17:14:21 +0100

    feat: get new releases and log first artist for latest release in console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer BQBJWUDM7RKI5N7Jdb87ROaSWnsQh-PW1NLGWUcNdUwTu7-_7nsW_0eW1bg3ENYDYiDqA1A_qoTt1X_Q716tsE3bXXEnWZiQ9w-RMJIaH7Qzlc-8jxuTwsRsXWUVIyi23MVdxsvUqG93CAGxigej");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    var latestArtist = fetch("https://api.spotify.com/v1/browse/new-releases", requestOptions)
+        .then(response => response.json())
+        .then(data => data["albums"]["items"][0]["artists"][0]["name"])
+        .catch(error => console.log('error', error));
+
+    console.log(latestArtist);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>New release by:</h1>
+                <p>latestArtist</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
